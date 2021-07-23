720e6eeebfda181f36d3b3b3e5de56a1a4af4024
limid404 <85405089+limid404@users.noreply.github.com>
2021-07-23 22:12:33 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -10,77 +10,68 @@ const token = '1928251630:AAHgX5MoSBZ3F0PACZyNucztqP2PT_eGUNA'
 const bot = new TelegramBot(token, {polling: true});
 
 // bots
-let state = 0;
-bot.onText(/\/start/, (msg) => {    
-    state = 0;
+state = 0;
+// Main Menu Bot
+bot.onText(/\/start/, (msg) => { 
     bot.sendMessage(
         msg.chat.id,
         `hello ${msg.chat.first_name}, welcome...\n
         click /predict`
-    );  
+    );   
+    state = 0;
 });
 
-
-bot.onText(/\/predict/, (msg) => { 
+bot.onText(/\/predict/, (msg) => {
     bot.sendMessage(
-        msg.chat.id,
-        `masukan nilai x1|x2|x3|x4 contohnya 9|9|9|9`
-    );   
+        msg.chat.id, 
+        `Masukan nilai x1, x2, x3, x4 dengan format x1|x2|x3|x4 \n
+        contohnya: 10|11|12|13`
+    );  
     state = 1;
 });
 
 bot.on('message', (msg) => {
+    const text = msg.text.toString().toLowerCase();
+    console.log(text);
+
     if(state == 1){
-        s = msg.text.split("|");
+        let dt = text.split('|');
+        bot.sendMessage(
+            msg.chat.id, 
+            `hasil output dengan nilai x1 (${dt[0]}), x2 (${dt[1]}), x3 (${dt[2]}), x4 (${dt[3]}) adalah y1 (${jres[0]}), y2 (${jres[1]}), y3 (${jres[2]}), y4 (${jres[3]}) y5 (${jres[4]}) dan y6 (${jres[5]}) `
+        );
+
         model.predict(
             [
-                parseFloat(s[0]), // string to float
-                parseFloat(s[1]),
-                parseFloat(s[2]),
-                parseFloat(s[3])
+                parseFloat(dt[0]), // string to float
+                parseFloat(dt[1]),
+                parseFloat(dt[2]),
+                parseFloat(dt[3])
             ]
-        ).then((jres1)=>{
-            console.log(jres1);
-            
-            cls_model.classify([parseFloat(s[0]), parseFloat(s[1]), parseFloat(s[2]), parseFloat(s[3]), parseFloat(jres1[0]), parseFloat(jres1[1]), parseFloat(jres1[2]), parseFloat(jres1[3]), parseFloat(jres1[4]), parseFloat(jres1[5]) ])=>{
-                bot.sendMessage(
-                    msg.chat.id,
-                    `nilai y1 yang diprediksi adalah ${jres1[0]}`
-                ); 
-                bot.sendMessage(
-                    msg.chat.id,
-                    `nilai y2 yang diprediksi adalah ${jres1[1]}`
-                ); 
-                bot.sendMessage(
-                    msg.chat.id,
-                    `nilai y3 yang diprediksi adalah ${jres1[2]}`
-                ); 
-                bot.sendMessage(
-                    msg.chat.id,
-                    `nilai y4 yang diprediksi adalah ${jres1[3]}`
-                ); 
-                bot.sendMessage(
-                    msg.chat.id,
-                    `nilai y5 yang diprediksi adalah ${jres1[4]}`
-                ); 
-                bot.sendMessage(
-                    msg.chat.id,
-                    `nilai y6 yang diprediksi adalah ${jres1[5]}`
-                ); 
-                state = 0;
-            })
-        })
-    }else{
+        ).then((jres) => {
+            bot.sendMessage(
+                msg.chat.id, 
+                `nilai y1 (${jres[0]}), y2 (${jres[1]}), y3 (${jres[2]}), y4 (${jres[3]}) y5 (${jres[4]}) dan y6 (${jres[5]})`
+            );
+            bot.sendMessage(
+                msg.chat.id,
+                `<= kembali /2`
+            );
+        });        
+    }
+
+    if(state == 2){
         bot.sendMessage(
-        msg.chat.id,
-            `Please Click /start`
-        );  
-        state = 0;
+            msg.chat.id, 
+            "pilih /start untuk ke menu utama"
+        );   
     }
 })
 
+
+
 // routers
-r.get('/predict/:x1/:x2/:x3/:x4', function(req, res, next) {    
+r.get('/prediction/:x1/:x2:/x3:/x4', function(req, res, next) {    
     model.predict(
         [
             parseFloat(req.params.x1), // string to float
@@ -93,33 +84,5 @@ r.get('/predict/:x1/:x2/:x3/:x4', function(req, res, next) {
     })
 });
 
-// routers
-r.get('/classify/:x1/:x2/:x3/:x4', function(req, res, next) {    
-    model.predict(
-        [
-            parseFloat(req.params.x1), // string to float
-            parseFloat(req.params.x2),
-            parseFloat(req.params.x3),
-            parseFloat(req.params.x4)
-        ]
-    ).then((jres)=>{
-        cls_model.classify(
-            [
-                parseFloat(req.params.x1), // string to float
-                parseFloat(req.params.x2),
-                parseFloat(req.params.x3),
-                parseFloat(req.params.x4),
-                parseFloat(jres[0]),
-                parseFloat(jres[1]),
-                parseFloat(jres[2]),
-                parseFloat(jres[3]),
-                parseFloat(jres[4]),
-                parseFloat(jres[5])
-            ]
-        ).then((jres_)=>{
-            res.json({jres, jres_})
-        })
-    })
-});
 
 module.exports = r;
