e9c33a06de5376cd7ee0a484772dc495824593b9
limid404 <85405089+limid404@users.noreply.github.com>
2021-07-23 22:27:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ bot.onText(/\/1/, (msg) => {
     state = 1;
     bot.sendMessage(
         msg.chat.id, 
-        `Masukan nilai i dan r dengan format x1|x2|x3|x4 \n
+        `Masukan nilai i dan r dengan format x1, x2, x3, x4 \n
         contohnya: 10|11|12|13`
     );   
 });
