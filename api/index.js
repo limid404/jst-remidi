2b753b35338e2f66c8fbfe56cb8394ee9ec01647
limid404 <85405089+limid404@users.noreply.github.com>
2021-07-23 22:21:08 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -29,60 +29,62 @@ bot.onText(/\/predict/, (msg) => {
     );  
     state = 1;
 });
-
 bot.on('message', (msg) => {
-    const text = msg.text.toString().toLowerCase();
-    console.log(text);
-
     if(state == 1){
-        let dt = text.split('|');
-        bot.sendMessage(
-            msg.chat.id, 
-            `hasil output dengan nilai x1 (${dt[0]}), x2 (${dt[1]}), x3 (${dt[2]}), x4 (${dt[3]}) adalah y1 (${jres[0]}), y2 (${jres[1]}), y3 (${jres[2]}), y4 (${jres[3]}) y5 (${jres[4]}) dan y6 (${jres[5]}) `
-        );
-
+        s = msg.text.split("|");
         model.predict(
             [
-                parseFloat(dt[0]), // string to float
-                parseFloat(dt[1]),
-                parseFloat(dt[2]),
-                parseFloat(dt[3])
-            ]
-        ).then((jres) => {
-            bot.sendMessage(
-                msg.chat.id, 
-                `nilai y1 (${jres[0]}), y2 (${jres[1]}), y3 (${jres[2]}), y4 (${jres[3]}) y5 (${jres[4]}) dan y6 (${jres[5]})`
-            );
-            bot.sendMessage(
-                msg.chat.id,
-                `<= kembali /2`
-            );
-        });        
-    }
-
-    if(state == 2){
-        bot.sendMessage(
-            msg.chat.id, 
-            "pilih /start untuk ke menu utama"
-        );   
-    }
+                parseFloat(s[0]), // string to float
+                parseFloat(s[1]),
+                parseFloat(s[2]),
+                parseFloat(s[3])
+                ]
+            ).then((jres1)=>{
+            console.log(jres1);
+                
+          model.predict([parseFloat(s[0]), parseFloat(s[1]), parseFloat(s[2]), parseFloat(s[3]), parseFloat(jres1[0]), parseFloat(jres1[1]), parseFloat(jres1[2])]), parseFloat(jres1[3]), parseFloat(jres1[4]), parseFloat(jres1[5])]) => {
+                bot.sendMessage(
+                    msg.chat.id,
+                    `nilai Y1 yang diprediksi adalah ${jres1[0]}`
+                    );
+                bot.sendMessage(
+                    msg.chat.id,
+                    `nilai Y2 yang diprediksi adalah ${jres1[1]}`
+                    );
+                bot.sendMessage(
+                    msg.chat.id,
+                    `nilai Y3 yang diprediksi adalah ${jres1[2]}`
+                     );
+                bot.sendMessage(
+                    msg.chat.id,
+                    `nilai Y4 yang diprediksi adalah ${jres1[3]}`
+                     );
+                 bot.sendMessage(
+                    msg.chat.id,
+                    `nilai Y5 yang diprediksi adalah ${jres1[4]}`
+                     );
+                  bot.sendMessage(
+                    msg.chat.id,
+                    `nilai Y6 yang diprediksi adalah ${jres1[5]}`
+                     );
+                        
+            })
+      }
+    state = 1;
 })
 
-
-
 // routers
-r.get('/prediction/:x1/:x2:/x3:/x4', function(req, res, next) {    
-    model.predict(
+r.get('/predict/:x1/:x2/:x3/:x4', function(req, res, next) {    
+            model.predict(
         [
             parseFloat(req.params.x1), // string to float
             parseFloat(req.params.x2),
             parseFloat(req.params.x3),
             parseFloat(req.params.x4)
         ]
-    ).then((jres)=>{
-        res.json(jres);
-    })
+    ).then((jres1)=>{
+       res.json(jres1)
+    })            
 });
-
 
 module.exports = r;
